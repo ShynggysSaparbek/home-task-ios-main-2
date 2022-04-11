502f74556beed095773d76df1f6d4dc686347565
ShynggysSaparbek <shynggys.saparbek@gmail.com>
2022-04-11 13:21:13 +0600

    Initial Commit

---
+++ home-task-ios/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  home-task-ios
+//
+//  Created by Sebastiano catellani on 17.05.21.
+//
+
+import UIKit
+
+ @main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    lazy var window: UIWindow? = UIWindow(frame: UIScreen.main.bounds)
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        self.window?.rootViewController = FeedViewController()
+        self.window?.makeKeyAndVisible()
+
+        return true
+    }
+}
